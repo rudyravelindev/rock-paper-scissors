438444c2fd26c66d4a62661c7c23699df3a89ded
rudyravelindev <rudyravelindev@gmail.com>
2024-09-03 00:08:54 -0700

    Get the computer choice function created and work well

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test")
+// computer choice variable
+const computerChoice = ["rock", "paper", "scissors"];
+
+// Get the computer choice
+function getComputerChoice(computerChoice){
+    const newComputerChoice = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[newComputerChoice];
+}
+
+console.log(getComputerChoice(computerChoice));
