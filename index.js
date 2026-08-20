a687a4e414739360033de8bdc9af5cd1c1da1976
rudyravelindev <rudyravelindev@gmail.com>
2026-08-19 23:29:44 -0700

    Get computer fucntion done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello World!');
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) {
+    return 'Rock';
+  } else if (computerChoice === 1) {
+    return 'Paper';
+  } else {
+    return 'Scissors';
+  }
+}
+console.log(getComputerChoice());
